16df8cdd65c00813594637fc4700744b5b4a3ca9
unkn0wn-hash <73510694+unkn0wn-hash@users.noreply.github.com>
2020-12-18 11:06:29 +0700

    Create ATRD_config.py

---
+++ ATRD_config.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+def trunking():
+	file = open("gsm.csv").readlines()
+	for line in file:
+		text  = line.strip().split('.')
+		print("""
+[GSM{0}]
+type=peer
+qualify=yes
+host={1}
+context=from-gsm
+nat=no
+disallow=all
+allow=ulaw
+allow=alaw
+""".format(text[2] + text[3],line.strip()))
+
+def gsm_config():
+	count=1
+	prefix=323
+
+	file = open("gsm.csv").readlines()
+	for line in file:
+		channel=line.split(',')[1].strip()
+		text=line.split(',')[0].strip().split('.')
+		print("""\
+GSM_TRUNK_VINA_{0}_{1}=SIP/GSM{2}
+GSM_TRUNK_VINA_{0}_{1}_MAX={3}
+GSM_TRUNK_VINA_{0}_{1}_TOTAL=0
+""".format(prefix,count,text[2] + text[3],channel))
+		count+=1
+	print("""\
+GSM_TOTAL_TRUNK_VINA_{0}={1}
+GSM_TOTAL_CALLS_VINA_{0}=0
+""".format(prefix,count-1))
+
+if __name__ == '__main__':
+	trunking()
