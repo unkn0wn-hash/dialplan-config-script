f2398ea30884f0da20bbe588eabb75431d9e2e8d
unkn0wn-hash <73510694+unkn0wn-hash@users.noreply.github.com>
2020-12-31 14:16:39 +0700

    Update ATRD_config.py

--- ATRD_config.py
+++ ATRD_config.py
@@ -3,7 +3,7 @@
 def trunking():
 	file = open("gsm.csv").readlines()
 	for line in file:
-		text  = line.strip().split('.')
+		text  = line.strip().split(',')[0].split('.')
 		print("""
 [GSM{0}]
 type=peer
@@ -13,27 +13,25 @@ context=from-gsm
 nat=no
 disallow=all
 allow=ulaw
-allow=alaw
-""".format(text[2] + text[3],line.strip()))
+allow=alaw""".format(text[2] + text[3],line.strip().split(',')[0]))
 
 def gsm_config():
 	count=1
-	prefix=323
+	prefix=340
 
 	file = open("gsm.csv").readlines()
 	for line in file:
 		channel=line.split(',')[1].strip()
 		text=line.split(',')[0].strip().split('.')
 		print("""\
-GSM_TRUNK_VINA_{0}_{1}=SIP/GSM{2}
-GSM_TRUNK_VINA_{0}_{1}_MAX={3}
-GSM_TRUNK_VINA_{0}_{1}_TOTAL=0
-""".format(prefix,count,text[2] + text[3],channel))
+GSM_TRUNK_MOBI_{0}_{1}=SIP/GSM{2}
+GSM_TRUNK_MOBI_{0}_{1}_MAX={3}
+GSM_TRUNK_MOBI_{0}_{1}_TOTAL=0""".format(prefix,count,text[2] + text[3],channel))
 		count+=1
 	print("""\
-GSM_TOTAL_TRUNK_VINA_{0}={1}
-GSM_TOTAL_CALLS_VINA_{0}=0
+GSM_TOTAL_TRUNK_MOBI_{0}={1}
+GSM_TOTAL_CALLS_MOBI_{0}=0
 """.format(prefix,count-1))
 
 if __name__ == '__main__':
-	trunking()
+	gsm_config()
